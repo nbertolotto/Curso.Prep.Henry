0ab02ce6f2856d62c2ae00067253f953fee401b9
nicolas Bertolotto <nbertolotto@gmail.com>
2022-01-06 19:44:14 +0100

    ultimo

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -202,12 +202,11 @@ function esVocal(letra){
   //Verificar si el usuario ingresó un string de más de un carácter, en ese caso, informarle 
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
-  //Escribe tu código aquí
-  
+  //Escribe tu código aquí NO TENIA IDEA DEL || hubiera hecho cinco if.
+  if (letra.lenght > 1){return "dato incorrecto"} if (letra==="a"|| letra === "e"|| letra === "i"|| letra === "o"|| letra==="U") {return "es vocal"} else {return "dato incurrecto"}
+
+
 }
-
-
-
 // No modificar nada debajo de esta línea
 // --------------------------------
 
