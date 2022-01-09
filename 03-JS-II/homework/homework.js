a47106ded238fb28a994183c39829da38f87c087
nicolas Bertolotto <nbertolotto@gmail.com>
2022-01-09 20:33:35 +0100

    JSII HM

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -77,14 +77,19 @@ function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (numero == 10) {return true;} else if (numero == 5) {return true;} else {return false;}
+  if (numero == 10) {
+    return true;} else if (numero == 5) {
+      return true;} else {
+        return false;}
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (numero> 20 && numero < 50) {return true;} else {return false;}
+  if (numero> 20 && numero < 50) {
+    return true;} else {
+      return false;}
 }
 
 function esEntero(numero) {
