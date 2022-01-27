f19e5d815de3cbfe1066ff9b51b38935c6b9620f
nicolas Bertolotto <nbertolotto@gmail.com>
2022-01-27 13:22:13 +0100

    jsv HM

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -4,11 +4,28 @@ function crearUsuario() {
   // Crea una Clase de ES6 o una función constructor llamada "Usuario"
   // Debe aceptar un objeto "opciones" con las propiedades "usuario", "nombre", "email" y "password"
   // En el `contructor`, define el usuario, el nombre, el email y la contraseña
-  // El `contructor` debe tener un método llamado "saludar" en su `prototype` que devuelva una string 'Hola, mi nombre es {{nombre}}'
+  // El `contructor` debe tener un método llamado "saludar" en su `prototype` que devuelva una string 
+  //'Hola, mi nombre es {{nombre}}'
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
-function Usuario(opciones){
+
+
+  //26/01:
+//function Usuario (opciones) { // recurdar que el Mayus da la nota a una clase o prototypo.
+  //this.usuario = opciones.usuario;
+  //this.nombre=opciones.nombre;
+  //this.email=opciones.email;
+  //this.password=opciones.password;
+//}
+//function saludar (){ return this.nombre;}// NOOOO
+  //Usuario.prototype.saludar = function(){
+    //return'Hola, mi nombre es ' + this.nombre; 
+  //}
+  //return Usuario;
+
+//}
+ function Usuario(opciones){
   this.usuario = opciones.usuario;
   this.nombre = opciones.nombre;
   this.email = opciones.email;
@@ -41,22 +58,29 @@ function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código: 
-  Constructor.prototype.saludar = function(){
-    return 'Hello World!';
-  };
   
-
-
+    //26/01: 
+  //Constructor.prototype.saludar = function(){
+    //return 'Hello World!';
+    //};
+  
+  Constructor.prototype.saludar= function () {
+    return 'Hello World!'};
 } 
 
 function agregarStringInvertida() {
-  // Agrega un método al prototype de String que devuelva la misma cadena de caracteres, pero invertida.
+  // Agrega un método al prototype de String que devuelva la misma cadena de caracteres,
+  // pero invertida.
   // El método debe llamarse "reverse"
   // Ej: 'menem'.reverse() => menem
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
 
-  //ESTA NO TENIA NI IDEA... AUNQUE TIENE SENTIDO... EL I -- NO SABIA..
+//26/01: 
+// String.prototye.reverse= function(){
+//for ( var i = this.length - 1; i==0 ;i--)  /// casi, me falto el -1 y el this.
+// reverso= reverso + this.charAt(i);   // habia hecho reverso[], reverso.push(this[i])
+//}; return reverso;
 
   String.prototype.reverse = function(){
     var stringInvertida = '';
@@ -78,6 +102,23 @@ function agregarStringInvertida() {
     //   Edad: 22,
     //   Domicilio: 'Saavedra 123'
     //  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   class Persona {
     constructor(nombre, apellido, edad, domicilio) {
@@ -101,7 +142,19 @@ function crearInstanciaPersona(nombre, apellido, edad, dir) {
   //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
   //Devolver la nueva persona creada
   // LA HICE CASI BIEN, ERROR PUESTO INGRESE LOS DATOS DIRECTAMENTE... JUAN, PEREZ, 22, SAAVEDRA
-var juan = new Persona(nombre,apellido,edad,dir);
+
+
+
+
+
+
+
+
+
+
+
+
+  var juan = new Persona(nombre,apellido,edad,dir);
 return juan;
 
 }
@@ -110,7 +163,21 @@ function agregarMetodo() {
   //La función agrega un método "datos" a la clase Persona que toma el nombre y la edad de la persona y devuelve: 
   //Ej: "Juan, 22 años"
   //LA HICE BIENN
-Persona.prototype.datos= function(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  Persona.prototype.datos= function(){
   return this.nombre+ ', '+ this.edad  + ' años';
 }
 
